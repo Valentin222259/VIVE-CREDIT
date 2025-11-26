5d94b1e82cabedc2ab0099ff8b27789d12e6471f
Stefan-Alexandru <stefan_a11@yahoo.com>
2025-11-26 08:13:01 +0100

    contract/loan validari si modificari adaugate (#25)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -8,6 +8,7 @@ import SuccessPage from "@/modules/onboarding/pages/SuccessPage";
 import OperatorDashboardLayout from "@/modules/operator-dashboard/layout/OperatorDashboardLayout";
 import OperatorDashboardPage from "@/modules/operator-dashboard/pages/OperatorDasboardPage";
 import RiskPage from "@/modules/operator-dashboard/pages/RiskPage";
+import LoanForm from "@/pages/loan/LoanForm";
 // import PolicyEnginePage from "@/modules/policy-engine/PolicyEnginePage";
 import { Route, Routes } from "react-router-dom";
 
@@ -15,22 +16,23 @@ const AppRoutes = () => {
   return (
     <Routes>
       {/* Root */}
-      <Route path='/' element={<div />} />
+      <Route path="/" element={<div />} />
 
       {/* Onboarding */}
-      <Route path='/onboarding' element={<OnboardingPage />} />
-      <Route path='/onboarding/success' element={<SuccessPage />} />
+      <Route path="/onboarding" element={<OnboardingPage />} />
+      <Route path="/onboarding/success" element={<SuccessPage />} />
 
       {/* Dashboard */}
-      <Route path='/dashboard' element={<DashboardPage />} />
-      <Route path='/dashboard/loan' element={<LoanPage />} />
-      <Route path='/dashboard/payments' element={<PaymentsPage />} />
-      <Route path='/dashboard/documents' element={<DocumentsPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/dashboard/loan" element={<LoanPage />} />
+      <Route path="/dashboard/payments" element={<PaymentsPage />} />
+      <Route path="/dashboard/documents" element={<DocumentsPage />} />
+      <Route path="/loan-form" element={<LoanForm />} />
 
       {/* OPERATOR DASHBOARD SALES/RISK/COLLECTIONS */}
-      <Route path='/operator' element={<OperatorDashboardLayout />}>
+      <Route path="/operator" element={<OperatorDashboardLayout />}>
         <Route index element={<OperatorDashboardPage />} />
-        <Route path='risk' element={<RiskPage />} />
+        <Route path="risk" element={<RiskPage />} />
         {/* <Route path="sales" element={<SalesPage />} /> */}
         {/* <Route path="collection" element={<CollectionPage />} /> */}
       </Route>
@@ -39,7 +41,7 @@ const AppRoutes = () => {
       {/* <Route path='/policy-engine' element={<PolicyEnginePage />} /> */}
 
       {/* Audit Dashboard */}
-      <Route path='/audit' element={<AuditDashboard />} />
+      <Route path="/audit" element={<AuditDashboard />} />
     </Routes>
   );
 };

5a923d28f20d452034186acb8ae1d3af4ebe1d9a
ardeleanvalentin <ardeleanvalentin490@yahoo.com>
2025-11-26 20:08:50 +0200

    Decision Engine

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,42 +1,36 @@
-import { AuditDashboard } from "@/modules/admin-audit/AuditDashboard";
 import DashboardPage from "@/modules/dashboard/pages/DashboardPage";
 import DocumentsPage from "@/modules/dashboard/pages/DocumentsPage";
 import LoanPage from "@/modules/dashboard/pages/LoanPage";
 import PaymentsPage from "@/modules/dashboard/pages/PaymentsPage";
-import { RiskDashboard } from "@modules/operator-dashboard/risk";
 import DecisionPage from "@/modules/decision-engine/Pages/DecisionPage";
 import OnboardingPage from "@/modules/onboarding/pages/OnboardingPage";
 import SuccessPage from "@/modules/onboarding/pages/SuccessPage";
-import DocumentsPage from "@/modules/dashboard/pages/DocumentsPage";
-import { RiskDashboard } from "@modules/operator-dashboard/risk";
 import { PolicyEnginePage } from "@/modules/scoring";
 import OperatorDashboardLayout from "@/modules/operator-dashboard/layout/OperatorDashboardLayout";
 import OperatorDashboardPage from "@/modules/operator-dashboard/pages/OperatorDasboardPage";
 import RiskPage from "@/modules/operator-dashboard/pages/RiskPage";
 import LoanForm from "@/pages/loan/LoanForm";
 // import PolicyEnginePage from "@/modules/policy-engine/PolicyEnginePage";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Root */}
       <Route path="/" element={<div />} />
-      
+
       {/* Onboarding */}
       <Route path="/onboarding" element={<OnboardingPage />} />
       <Route path="/onboarding/success" element={<SuccessPage />} />
-      
+
       {/* Dashboard */}
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/dashboard/loan" element={<LoanPage />} />
       <Route path="/dashboard/payments" element={<PaymentsPage />} />
       <Route path="/dashboard/documents" element={<DocumentsPage />} />
-      <Route path="/risk" element={<RiskDashboard />} />
       <Route path="/decision-engine" element={<DecisionPage />} />
       <Route path="/loan-form" element={<LoanForm />} />
 
-      
       {/* OPERATOR DASHBOARD SALES/RISK/COLLECTIONS */}
       <Route path="/operator" element={<OperatorDashboardLayout />}>
         <Route index element={<OperatorDashboardPage />} />
@@ -44,10 +38,10 @@ const AppRoutes = () => {
         {/* <Route path="sales" element={<SalesPage />} /> */}
         {/* <Route path="collection" element={<CollectionPage />} /> */}
       </Route>
-      
+
       {/* Policy Engine */}
       <Route path="/policy-engine" element={<PolicyEnginePage />} />
-      
+
       {/* Fallback */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
